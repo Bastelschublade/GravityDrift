ee26a51f2bdd6967f44f895f6edb938d42b85199
sarbot <sarbot@finchen.localdomain>
2017-03-12 00:59:33 +0100

    upload init libgdx project

---
+++ html/src/de/sarbot/gravitydrift/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.sarbot.gravitydrift.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.sarbot.gravitydrift.GravityDrift;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GravityDrift();
+        }
+}
